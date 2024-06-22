3348e63a06f104ca2180a3270b28535a1481c202
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2024-06-22 14:26:53 +0530

    Add grid lines to the canvas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 const CANVAS_DIMENSIONS = 400;
 const CANVAS_BACKGROUND = "#3b3b3b";
+const CANVAS_LINES = "#ffffff";
 const GRID_ROWS = 10;
 const GRID_COLUMNS = 10;
 const gameCanvas = document.getElementById("game");
@@ -16,4 +17,18 @@ if (context === null) {
 }
 context.fillStyle = CANVAS_BACKGROUND;
 context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+context.strokeStyle = CANVAS_LINES;
 // console.log({ context });
+const cell_width = context.canvas.width / GRID_COLUMNS;
+const cell_height = context.canvas.height / GRID_ROWS;
+for (let x = 0; x <= GRID_COLUMNS; x++) {
+    context.moveTo(x * cell_width, 0);
+    context.lineTo(x * cell_width, context.canvas.height);
+    // context.stroke();
+}
+for (let y = 0; y <= GRID_ROWS; y++) {
+    context.moveTo(0, y * cell_height);
+    context.lineTo(context.canvas.width, y * cell_height);
+    // context.stroke();
+}
+context.stroke();
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 const CANVAS_DIMENSIONS = 400;
 const CANVAS_BACKGROUND = "#3b3b3b";
+const CANVAS_LINES = "#ffffff";
 
 const GRID_ROWS = 10;
 const GRID_COLUMNS = 10;
@@ -21,4 +22,22 @@ if (context === null) {
 
 context.fillStyle = CANVAS_BACKGROUND;
 context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+context.strokeStyle = CANVAS_LINES;
 // console.log({ context });
+
+const cell_width = context.canvas.width / GRID_COLUMNS;
+const cell_height = context.canvas.height / GRID_ROWS;
+
+for (let x = 0; x <= GRID_COLUMNS; x++) {
+	context.moveTo(x * cell_width, 0);
+	context.lineTo(x * cell_width, context.canvas.height);
+	// context.stroke();
+}
+
+for (let y = 0; y <= GRID_ROWS; y++) {
+	context.moveTo(0, y * cell_height);
+	context.lineTo(context.canvas.width, y * cell_height);
+	// context.stroke();
+}
+
+context.stroke();
